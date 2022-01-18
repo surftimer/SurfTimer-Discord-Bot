de924eef4acc7f3f3ed5304508f3e7f19d2002d8
Sarrus1 <omegasix0611@gmail.com>
2022-01-19 00:20:17 +0100

    first commit

---
+++ src/commands/test.ts
@@ -0,0 +1,20 @@
+﻿const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!')
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+
+export function reg(){
+  rest.put(Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+}
---
+++ src/main.ts
@@ -0,0 +1,42 @@
+﻿import { Client, Intents } from "discord.js";
+import * as dotenv from "dotenv";
+import {reg} from "./commands/test";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+dotenv.config();
+
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+reg();
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+    const map = await prisma.ck_maptier.findUnique({
+      where: {
+        mapname: "surf_004_fix",
+      },
+      select: {
+        mapname: true,
+        tier: true
+      },
+    })
+    console.dir(map, { depth: null });
+		await interaction.reply(map.tier.toString());
+	}
+});
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN);
