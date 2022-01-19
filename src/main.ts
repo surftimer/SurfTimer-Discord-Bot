aae584dc30b1ba5b501bb5fed69f19a121b49586
Sarrus1 <omegasix0611@gmail.com>
2022-01-19 13:40:35 +0100

    Chore: formatting

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,40 @@
 ﻿import { Client, Intents } from "discord.js";
 import * as dotenv from "dotenv";
-import {reg} from "./commands/test";
-import { PrismaClient } from '@prisma/client'
+import { reg } from "./commands/test";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 dotenv.config();
-
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
 reg();
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+  const { commandName } = interaction;
 
-	if (commandName === 'ping') {
+  if (commandName === "ping") {
     const map = await prisma.ck_maptier.findUnique({
       where: {
         mapname: "surf_004_fix",
       },
       select: {
         mapname: true,
-        tier: true
+        tier: true,
       },
-    })
+    });
     console.dir(map, { depth: null });
-		await interaction.reply(map.tier.toString());
-	}
+    await interaction.reply(map.tier.toString());
+  }
 });
 
 // Login to Discord with your client's token
